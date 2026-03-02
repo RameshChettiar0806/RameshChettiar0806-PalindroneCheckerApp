7e5b42710a63ee026ec9b410a4b7af80d70c35f4
RameshChettiar0806 <rameshchettiar06@gmail.com>
2026-03-02 14:19:55 +0530

    UC09 - - Recursive Palindrome Checker

---
+++ App/src/UC09PallindromeCheckerApp.java
@@ -0,0 +1,68 @@
+/**
+ * MAIN CLASS - UseCase9RecursivePalindrome
+ *
+ * Use Case 9: Recursive Palindrome Checker
+ *
+ * Description:
+ * This class validates whether a string is a palindrome using recursion.
+ *
+ * Characters are compared from the outer positions
+ * moving inward using recursive calls.
+ *
+ * The recursion stops when:
+ * - All characters are matched, or
+ * - A mismatch is found.
+ *
+ * This use case demonstrates divide-and-conquer
+ * logic using method recursion.
+ *
+ * @author Ramesh
+ * @version 9.0
+ */
+import java.util.Scanner;
+
+public class UC09PallindromeCheckerApp {
+
+    /**
+     * Application entry point for UC9.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String -> ");
+        String input = sc.nextLine();
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+        sc.close();
+    }
+
+    /**
+     * Recursively checks whether a string is a palindrome.
+     *
+     * @param s     Input string
+     * @param start Starting index
+     * @param end   Ending index
+     * @return true if palindrome, otherwise false
+     */
+    private static boolean check(String s, int start, int end) {
+
+        // Base Condition: If start crosses or meets end, all characters matched
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters at start and end do not match, not a palindrome
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call moving inward
+        return check(s, start + 1, end - 1);
+
+
+    }
+}
